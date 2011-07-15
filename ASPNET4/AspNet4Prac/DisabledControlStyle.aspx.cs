46bc1bc6809fa121ec8f07ada1d099de0a4ff911
darshanm <darshanm@mum02d2124.gspl.com>
2011-07-15 12:57:58 +0530

    First Check - in

---
+++ ASPNET4/AspNet4Prac/DisabledControlStyle.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNet4Prac
+{
+    public partial class DisabledControlStyle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEnable_Click(object sender, EventArgs e)
+        {
+            txtControl.Text = "Control Is Enabled";
+            txtControl.Enabled = true;
+
+        }
+
+        protected void btnDisable_Click(object sender, EventArgs e)
+        {
+            txtControl.Text = "Control Is Disabled";
+            txtControl.Enabled = false;
+        }
+    }
+}
